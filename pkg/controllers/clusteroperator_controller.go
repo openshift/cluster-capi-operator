0fbb93616b1e57c215b76187bce1ee37b37f296d
Alexander Demichev <demichev.alexander@gmail.com>
2021-09-16 17:59:25 +0200

    Setup cluster operator controller

---
+++ pkg/controllers/clusteroperator_controller.go
@@ -0,0 +1,71 @@
+package controllers
+
+import (
+	"context"
+
+	configv1 "github.com/openshift/api/config/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/klog"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+// ClusterOperatorReconciler reconciles a ClusterOperator object
+type ClusterOperatorReconciler struct {
+	client.Client
+	Scheme           *runtime.Scheme
+	Recorder         record.EventRecorder
+	ReleaseVersion   string
+	ManagedNamespace string
+	ImagesFile       string
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ClusterOperatorReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&configv1.ClusterOperator{}).
+		Watches(
+			&source.Kind{Type: &configv1.Infrastructure{}},
+			handler.EnqueueRequestsFromMapFunc(toClusterOperator),
+			builder.WithPredicates(infrastructurePredicates()),
+		).
+		Watches(
+			&source.Kind{Type: &configv1.FeatureGate{}},
+			handler.EnqueueRequestsFromMapFunc(toClusterOperator),
+			builder.WithPredicates(featureGatePredicates()),
+		).
+		Complete(r)
+}
+
+// +kubebuilder:rbac:groups=config.openshift.io,resources=clusteroperators,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=config.openshift.io,resources=clusteroperators/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=config.openshift.io,resources=clusteroperators/finalizers,verbs=update
+
+// Reconcile will process the cluster-api clusterOperator
+func (r *ClusterOperatorReconciler) Reconcile(ctx context.Context, _ ctrl.Request) (ctrl.Result, error) {
+	featureGate := &configv1.FeatureGate{}
+	if err := r.Get(ctx, client.ObjectKey{Name: externalFeatureGateName}, featureGate); errors.IsNotFound(err) {
+		klog.Infof("FeatureGate cluster does not exist. Skipping...")
+		return ctrl.Result{}, r.setStatusAvailable(ctx)
+	} else if err != nil {
+		klog.Errorf("Unable to retrive FeatureGate object: %v", err)
+		return ctrl.Result{}, r.setStatusDegraded(ctx, err)
+	}
+
+	// Verify FeatureGate ClusterAPIEnabled is present for operator to work in TP phase
+	capiEnabled, err := isCAPIFeatureGateEnabled(featureGate)
+	if err != nil {
+		klog.Errorf("Could not determine cluster api feature gate state: %v", err)
+		return ctrl.Result{}, r.setStatusDegraded(ctx, err)
+	} else if !capiEnabled {
+		klog.Infof("FeatureGate cluster does not include cluster api. Skipping...")
+		return ctrl.Result{}, r.setStatusAvailable(ctx)
+	}
+
+	return ctrl.Result{}, nil
+}
---
+++ pkg/controllers/common_consts.go
@@ -0,0 +1,14 @@
+package controllers
+
+const (
+	DefaultManagedNamespace = "openshift-cluster-api"
+
+	infrastructureResourceName = "cluster"
+	clusterOperatorName        = "cluster-api"
+	operatorVersionKey         = "operator"
+	externalFeatureGateName    = "cluster"
+
+	// ClusterAPIEnabled is the name of the cluster API feature gate.
+	// This is used to enable the cluster API.
+	ClusterAPIEnabled = "ClusterAPIEnabled"
+)
