92640a41213ee2ea580f049bcf0e321f15da6bb4
Mike Fedosin <mfedosin@redhat.com>
2022-10-17 14:20:41 +0200

    Add testing tools to vendor folder
    Currently we fetch the binaries during CI runs, but it's better to
    put it in the vendor folder and execute directly.
    
    This commit puts 3 tools in the vendor folder:
    - golangci-lint
    - ginkgo
    - envtest

---
+++ tools.go
@@ -0,0 +1,9 @@
+//go:build tools
+// +build tools
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/onsi/ginkgo/v2/ginkgo"
+	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
+)
