91b071a9870860599947948c8311638f25263dd9
Daniel Odvarka <odvarkaadaniel@gmail.com>
2023-07-20 14:22:35 +0200

    Create VSphere secret for VSphereCluster in the helper script

--- hack/clusters/create-vsphere.sh
+++ hack/clusters/create-vsphere.sh
@@ -12,6 +12,9 @@ export INFRASTRUCTURE_KIND=VSphereCluster
 export CONTROL_PLANE_ENDPOINT_IP=$(kubectl get infrastructure cluster -o jsonpath="{.status.platformStatus.vsphere.apiServerInternalIP}")
 export VSPHERE_SERVER=$(kubectl get infrastructure cluster -o jsonpath="{.spec.platformSpec.vsphere.vcenters[0].server}")
 
+printcolor "Creating VSphere secret for VSphereCluster"
+envsubst <hack/clusters/templates/secret_vsphere.yaml | kubectl apply -f -
+
 printcolor "Creating VSphere infrastructure cluster"
 envsubst <hack/clusters/templates/vsphere.yaml | kubectl apply -f -
 
