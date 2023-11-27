b88e1c4b00169414f53abfb4afc8dafe46f2615e
Daniel Odvarka <odvarkaadaniel@gmail.com>
2023-11-27 18:08:01 +0100

    Parse the APIServerURL using url parser

--- e2e/framework/util.go
+++ e2e/framework/util.go
@@ -1,8 +1,9 @@
 package framework
 
 import (
+	"fmt"
+	"net/url"
 	"strconv"
-	"strings"
 
 	configv1 "github.com/openshift/api/config/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -16,19 +17,18 @@ func GetControlPlaneHostAndPort(cl client.Client) (string, int32, error) {
 	}
 
 	if err := cl.Get(ctx, namespacedName, &infraCluster); err != nil {
-		return "", 0, err
+		return "", 0, fmt.Errorf("failed to get the infrastructure object: %w", err)
 	}
 
-	hostAndPort := strings.TrimPrefix(infraCluster.Status.APIServerURL, "https://")
-	apiEndpoint := strings.Split(hostAndPort, ":")
-	if len(apiEndpoint) < 2 {
-		return apiEndpoint[0], 0, nil
-	}
-
-	port, err := strconv.ParseInt(apiEndpoint[1], 10, 32)
+	apiUrl, err := url.Parse(infraCluster.Status.APIServerURL)
 	if err != nil {
-		return apiEndpoint[0], 0, nil
+		return "", 0, fmt.Errorf("failed to parse the API server URL: %w", err)
 	}
 
-	return apiEndpoint[0], int32(port), nil
+	port, err := strconv.ParseInt(apiUrl.Port(), 10, 32)
+	if err != nil {
+		return apiUrl.Hostname(), 0, fmt.Errorf("failed to parse port: %w", err)
+	}
+
+	return apiUrl.Hostname(), int32(port), nil
 }
