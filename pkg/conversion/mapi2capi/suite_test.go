002c9e940dbd30aa9cac975935d57bbb5200660d
Joel Speed <joel.speed@hotmail.co.uk>
2024-09-23 17:55:41 +0100

    Add AWS MAPI2CAPI fuzz testing

--- pkg/conversion/mapi2capi/suite_test.go
+++ pkg/conversion/mapi2capi/suite_test.go
@@ -13,16 +13,33 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package mapi2capi
+package mapi2capi_test
 
 import (
+	"fmt"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	kubescheme "k8s.io/client-go/kubernetes/scheme"
+
+	mapiv1 "github.com/openshift/api/machine/v1beta1"
 )
 
-func TestAPIs(t *testing.T) {
+var scheme *runtime.Scheme
+
+func init() {
+	// Register the scheme for the test.
+	// This must be done before the tests are run as the fuzzer is needed before the test tree is compiled.
+	scheme = kubescheme.Scheme
+	if err := mapiv1.AddToScheme(scheme); err != nil {
+		panic(fmt.Sprintf("failed to add machine scheme: %v", err))
+	}
+}
+
+func TestFuzz(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "MAPI2CAPI Suite")
+	RunSpecs(t, "MAPI2CAPI Fuzz Suite")
 }
