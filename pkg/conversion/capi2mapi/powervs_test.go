216f70404bf455592b94e85040701b698c3f13fe
Karthik Bhat <karthikkn1997@gmail.com>
2024-10-09 14:54:50 +0530

    Add unit test for Power VS CAPI-MAPI conversion

---
+++ pkg/conversion/capi2mapi/powervs_test.go
@@ -0,0 +1,92 @@
+/*
+Copyright 2024 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package capi2mapi
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/utils/ptr"
+	capibmv1 "sigs.k8s.io/cluster-api-provider-ibmcloud/api/v1beta2"
+	capiv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("capi2mapi PowerVS", func() {
+
+	powerVSMachineTemplate := &capibmv1.IBMPowerVSMachineTemplate{
+		Spec: capibmv1.IBMPowerVSMachineTemplateSpec{
+			Template: capibmv1.IBMPowerVSMachineTemplateResource{
+				Spec: capibmv1.IBMPowerVSMachineSpec{
+					ImageRef:   &corev1.LocalObjectReference{Name: "rhcos-capi-powervs"},
+					ProviderID: ptr.To("test-123"),
+				},
+			},
+		},
+		Status: capibmv1.IBMPowerVSMachineTemplateStatus{},
+	}
+
+	powerVSMachine := &capibmv1.IBMPowerVSMachine{
+		Spec: capibmv1.IBMPowerVSMachineSpec{
+			ImageRef:   &corev1.LocalObjectReference{Name: "rhcos-capi-powervs"},
+			ProviderID: ptr.To("test-123"),
+		},
+	}
+
+	capiMachine := &capiv1.Machine{
+		Spec: capiv1.MachineSpec{
+			ClusterName: "test-123",
+		},
+	}
+
+	powerVSMachineSet := &capiv1.MachineSet{
+		Spec: capiv1.MachineSetSpec{
+			Replicas:    ptr.To(int32(2)),
+			ClusterName: "test-123",
+			Template: capiv1.MachineTemplateSpec{
+				Spec: capiv1.MachineSpec{
+					ClusterName: "test-123",
+				},
+			},
+		},
+	}
+
+	capiPowerVSCluster := &capibmv1.IBMPowerVSCluster{
+		Spec: capibmv1.IBMPowerVSClusterSpec{
+			ServiceInstance: &capibmv1.IBMPowerVSResourceReference{Name: ptr.To("serviceInstance")},
+			Zone:            ptr.To("test-zone"),
+		},
+		Status: capibmv1.IBMPowerVSClusterStatus{
+			Ready: true,
+		},
+	}
+
+	It("should be able to convert a Power VS CAPI Machine to a Power VS MAPI Machine", func() {
+		mapiMachine, warns, err :=
+			FromMachineAndPowerVSMachineAndPowerVSCluster(capiMachine, powerVSMachine, capiPowerVSCluster).ToMachine()
+		Expect(mapiMachine).To(Not(BeNil()), "should not have a nil MAPI Machine")
+		Expect(err).ToNot(HaveOccurred(), "should have been able to convert CAPI Machine/PowerVSMachineTemplate to MAPI Machine")
+		Expect(warns).To(BeEmpty(), "should have not warned while converting CAPI Machine/PowerVSMachineTemplate to MAPI Machine")
+	})
+
+	It("should be able to convert a Power VS MAPI MachineSet to a Power VS CAPI MachineSet", func() {
+		mapiMachineSet, warns, err :=
+			FromMachineSetAndPowerVSMachineTemplateAndPowerVSCluster(powerVSMachineSet, powerVSMachineTemplate, capiPowerVSCluster).ToMachineSet()
+		Expect(mapiMachineSet).To(Not(BeNil()), "should not have a nil MAPI MachineSet")
+		Expect(err).ToNot(HaveOccurred(), "should have been able to convert CAPI MachineSet/PowerVSMachineTemplate to MAPI MachineSet")
+		Expect(warns).To(BeEmpty(), "should have not warned while converting CAPI MachineSet/PowerVSMachineTemplate to MAPI MachineSet")
+	})
+})
---
+++ pkg/conversion/mapi2capi/powervs_test.go
@@ -0,0 +1,142 @@
+/*
+Copyright 2024 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package mapi2capi
+
+import (
+	"encoding/json"
+	"fmt"
+	mapiv1 "github.com/openshift/api/machine/v1"
+	machinev1beta1 "github.com/openshift/api/machine/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/utils/ptr"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	configv1 "github.com/openshift/api/config/v1"
+)
+
+var _ = Describe("mapi2capi PowerVS", func() {
+
+	powerVSProviderSpec := getPowerVSProviderSpec()
+
+	powerVSRawExt, err := rawExtensionFromPowerVSProviderSpec(powerVSProviderSpec)
+	Expect(err).ToNot(HaveOccurred())
+
+	powerVSMAPIMachine := &machinev1beta1.Machine{
+		Spec: machinev1beta1.MachineSpec{
+			ProviderSpec: machinev1beta1.ProviderSpec{
+				Value: powerVSRawExt,
+			},
+			ProviderID: ptr.To("test-123"),
+		},
+	}
+
+	powerVSMAPIMachineSet := &machinev1beta1.MachineSet{
+		Spec: machinev1beta1.MachineSetSpec{
+			Selector: metav1.LabelSelector{},
+			Template: machinev1beta1.MachineTemplateSpec{
+				Spec: machinev1beta1.MachineSpec{
+					ProviderSpec: machinev1beta1.ProviderSpec{
+						Value: powerVSRawExt,
+					},
+					ProviderID: ptr.To("test-123"),
+				},
+			},
+		},
+	}
+
+	infra := configv1.Infrastructure{
+		Spec: configv1.InfrastructureSpec{},
+		Status: configv1.InfrastructureStatus{
+			InfrastructureName: "sample-cluster-name",
+		},
+	}
+
+	// We need to add error handling for the providerSpec issues before we can enable the following tests.
+	PIt("should be able to convert a MAPI Power VS Machine to a CAPI Machine", func() {
+		// Convert a MAPI Machine to a CAPI Core Machine + a CAPI InfraMachine.
+		capiMachine, capiInfraMachine, warns, err :=
+			FromPowerVSMachineAndInfra(powerVSMAPIMachine, &infra).ToMachineAndInfrastructureMachine()
+		fmt.Print(capiMachine)
+		Expect(err).ToNot(HaveOccurred(), "should have been able to convert providerSpec to MachineTemplateSpec")
+		Expect(warns).To(BeEmpty(), "should have not warned while converting providerSpec to MachineTemplateSpec")
+		Expect(capiMachine).To(Not(BeNil()), "should not have a nil CAPI Machine")
+		Expect(capiInfraMachine).To(Not(BeNil()), "should not have a nil CAPI InfrastructureMachine")
+	})
+
+	It("should be able to convert a MAPI Power VS MachineSet to a CAPI MachineSet", func() {
+		// Convert a MAPI MachineSet to a CAPI Core MachineSet + a CAPI InfraMachineTemplateSpec.
+		capiMachineSet, capiInfraMachineTemplate, warns, err :=
+			FromPowerVSMachineSetAndInfra(powerVSMAPIMachineSet, &infra).ToMachineSetAndMachineTemplate()
+		Expect(err).ToNot(HaveOccurred(), "should have been able to convert MAPI MachineSet to CAPI MachineSet")
+		Expect(warns).To(BeEmpty(), "should have not warned while converting MAPI MachineSet to CAPI MachineSet")
+		Expect(capiMachineSet).To(Not(BeNil()), "should not have a nil CAPI MachineSet")
+		Expect(capiInfraMachineTemplate).To(Not(BeNil()), "should not have a nil CAPI MachineTemplate")
+	})
+
+})
+
+// getPowerVSProviderSpec builds and returns PowerVSProviderConfig
+func getPowerVSProviderSpec() *mapiv1.PowerVSMachineProviderConfig {
+	// TODO: May be we can add this in machine builder?
+	return &mapiv1.PowerVSMachineProviderConfig{
+		TypeMeta:   metav1.TypeMeta{},
+		ObjectMeta: metav1.ObjectMeta{},
+		UserDataSecret: &mapiv1.PowerVSSecretReference{
+			Name: "worker-user-data",
+		},
+		CredentialsSecret: &mapiv1.PowerVSSecretReference{
+			Name: "powervs-credentials",
+		},
+		ServiceInstance: mapiv1.PowerVSResource{
+			Type:  mapiv1.PowerVSResourceTypeID,
+			ID:    ptr.To("1234"),
+			Name:  nil,
+			RegEx: nil,
+		},
+		Image: mapiv1.PowerVSResource{
+			Type: mapiv1.PowerVSResourceTypeName,
+			Name: ptr.To("rhcos-ipi-sa04-418nig-jqw97"),
+		},
+		Network: mapiv1.PowerVSResource{
+			Type:  mapiv1.PowerVSResourceTypeRegEx,
+			RegEx: ptr.To("^DHCPSERVER.*ipi-sa04-418nig-jqw97.*_Private$"),
+		},
+		KeyPairName:   "ipi-sa04-418nig-jqw97-key",
+		SystemType:    "s922",
+		ProcessorType: mapiv1.PowerVSProcessorTypeShared,
+		Processors:    intstr.FromString("2"),
+		MemoryGiB:     32,
+	}
+}
+
+// rawExtensionFromPowerVSProviderSpec marshals the machine provider spec.
+func rawExtensionFromPowerVSProviderSpec(spec *mapiv1.PowerVSMachineProviderConfig) (*runtime.RawExtension, error) {
+	if spec == nil {
+		return &runtime.RawExtension{}, nil
+	}
+
+	rawBytes, err := json.Marshal(spec)
+	if err != nil {
+		return nil, fmt.Errorf("error marshalling providerSpec: %w", err)
+	}
+
+	return &runtime.RawExtension{
+		Raw: rawBytes,
+	}, nil
+}
