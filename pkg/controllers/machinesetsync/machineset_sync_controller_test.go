cf279856233d92613447c924308e0a06623a93d3
Theo Barber-Bany <theobarberbany@gmail.com>
2024-10-23 19:40:19 +0100

    Updates tests

--- pkg/controllers/machinesetsync/machineset_sync_controller_test.go
+++ pkg/controllers/machinesetsync/machineset_sync_controller_test.go
@@ -21,29 +21,48 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
 	configv1 "github.com/openshift/api/config/v1"
 	machinev1beta1 "github.com/openshift/api/machine/v1beta1"
+	capiv1resourcebuilder "github.com/openshift/cluster-api-actuator-pkg/testutils/resourcebuilder/cluster-api/core/v1beta1"
+	capav1builder "github.com/openshift/cluster-api-actuator-pkg/testutils/resourcebuilder/cluster-api/infrastructure/v1beta2"
 	corev1resourcebuilder "github.com/openshift/cluster-api-actuator-pkg/testutils/resourcebuilder/core/v1"
 	machinev1resourcebuilder "github.com/openshift/cluster-api-actuator-pkg/testutils/resourcebuilder/machine/v1beta1"
 	corev1 "k8s.io/api/core/v1"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
+	capav1 "sigs.k8s.io/cluster-api-provider-aws/v2/api/v1beta2"
+	capiv1beta1 "sigs.k8s.io/cluster-api/api/v1beta1"
 
 	consts "github.com/openshift/cluster-capi-operator/pkg/controllers"
 	"github.com/openshift/cluster-capi-operator/pkg/test"
+	"k8s.io/utils/ptr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/config"
+	"sigs.k8s.io/controller-runtime/pkg/envtest/komega"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
-var _ = Describe("MachineSetSync Reconciler", func() {
+var _ = Describe("With a running controller", func() {
 	var mgrCancel context.CancelFunc
 	var mgrDone chan struct{}
 	var mgr manager.Manager
+	var k komega.Komega
 	var reconciler *MachineSetSyncReconciler
 
-	var namespace *corev1.Namespace
-	var namespaceName string
+	var syncControllerNamespace *corev1.Namespace
+	var capiNamespace *corev1.Namespace
+	var MapiNamespace *corev1.Namespace
 
-	var machineSetBuilder machinev1resourcebuilder.MachineSetBuilder
-	var machineset *machinev1beta1.MachineSet
+	var mapiMachineSetBuilder machinev1resourcebuilder.MachineSetBuilder
+	var mapiMachineSet *machinev1beta1.MachineSet
+
+	var capiMachineSetBuilder capiv1resourcebuilder.MachineSetBuilder
+	var capiMachineSet *capiv1beta1.MachineSet
+
+	var capaMachineTemplateBuilder capav1builder.AWSMachineTemplateBuilder
+	var capaMachineTemplate *capav1.AWSMachineTemplate
+
+	var capaClusterBuilder capav1builder.AWSClusterBuilder
+	var capaCluster *capav1.AWSCluster
 
 	startManager := func(mgr *manager.Manager) (context.CancelFunc, chan struct{}) {
 		mgrCtx, mgrCancel := context.WithCancel(context.Background())
@@ -67,32 +86,82 @@ var _ = Describe("MachineSetSync Reconciler", func() {
 
 	BeforeEach(func() {
 		By("Setting up a namespace for the test")
-		namespace = corev1resourcebuilder.Namespace().WithGenerateName("machineset-sync-controller-").Build()
-		Expect(k8sClient.Create(ctx, namespace)).To(Succeed())
-		namespaceName = namespace.GetName()
+		syncControllerNamespace = corev1resourcebuilder.Namespace().
+			WithGenerateName("machineset-sync-controller-").Build()
+		Expect(k8sClient.Create(ctx, syncControllerNamespace)).To(Succeed())
 
-		By("Setting up the machineset builder")
-		machineSetBuilder = machinev1resourcebuilder.MachineSet().
-			WithNamespace(namespaceName).
-			WithGenerateName("foo-").
+		MapiNamespace = corev1resourcebuilder.Namespace().
+			WithGenerateName("openshift-machine-api-").Build()
+		Expect(k8sClient.Create(ctx, MapiNamespace)).To(Succeed())
+
+		capiNamespace = corev1resourcebuilder.Namespace().
+			WithGenerateName("openshift-cluster-api-").Build()
+		Expect(k8sClient.Create(ctx, capiNamespace)).To(Succeed())
+
+		By("Setting up the builders")
+		mapiMachineSetBuilder = machinev1resourcebuilder.MachineSet().
+			WithNamespace(MapiNamespace.GetName()).
+			WithName("foo").
 			WithProviderSpecBuilder(machinev1resourcebuilder.AWSProviderSpec())
+
+		// We need to build and create the CAPA MachineTemplate in order to
+		// reference it on the CAPI MachineSet
+		capaMachineTemplateBuilder = capav1builder.AWSMachineTemplate().
+			WithNamespace(capiNamespace.GetName()).
+			WithGenerateName("machine-template-")
+
+		capaMachineTemplate = capaMachineTemplateBuilder.Build()
+		Expect(k8sClient.Create(ctx, capaMachineTemplate)).To(Succeed())
+
+		capiMachineTemplate := capiv1beta1.MachineTemplateSpec{
+			Spec: capiv1beta1.MachineSpec{
+				InfrastructureRef: corev1.ObjectReference{
+					Kind:      capaMachineTemplate.Kind,
+					Name:      capaMachineTemplate.GetName(),
+					Namespace: capaMachineTemplate.GetNamespace(),
+				},
+			},
+		}
+
+		capaClusterBuilder = capav1builder.AWSCluster().
+			WithNamespace(capiNamespace.GetName()).
+			WithGenerateName("foo-")
+
+		capaCluster = capaClusterBuilder.Build()
+		Expect(k8sClient.Create(ctx, capaCluster)).To(Succeed())
+
+		capiMachineSetBuilder = capiv1resourcebuilder.MachineSet().
+			WithNamespace(capiNamespace.GetName()).
+			WithName("foo").
+			WithTemplate(capiMachineTemplate).
+			WithClusterName(capaCluster.GetName())
 
 		By("Setting up a manager and controller")
 		var err error
 		mgr, err = ctrl.NewManager(cfg, ctrl.Options{
 			Scheme: testScheme,
+			Controller: config.Controller{
+				SkipNameValidation: ptr.To(true),
+			},
 		})
 		Expect(err).ToNot(HaveOccurred(), "Manager should be able to be created")
 
 		reconciler = &MachineSetSyncReconciler{
-			Client:   mgr.GetClient(),
-			Platform: configv1.AWSPlatformType,
+			Client:        mgr.GetClient(),
+			Platform:      configv1.AWSPlatformType,
+			CAPINamespace: capiNamespace.GetName(),
+			MAPINamespace: MapiNamespace.GetName(),
 		}
-		Expect(reconciler.SetupWithManager(mgr)).To(Succeed(), "Reconciler should be able to setup with manager")
+		Expect(reconciler.SetupWithManager(mgr)).To(Succeed(),
+			"Reconciler should be able to setup with manager")
+
+		k = komega.New(k8sClient)
 	})
 
 	AfterEach(func() {
-		Expect(test.CleanupAndWait(ctx, k8sClient, machineset)).To(Succeed())
+		Expect(test.CleanupAndWait(
+			ctx, k8sClient, mapiMachineSet, capiMachineSet, capaMachineTemplate, capaCluster,
+		)).To(Succeed())
 	})
 
 	JustBeforeEach(func() {
@@ -106,32 +175,37 @@ var _ = Describe("MachineSetSync Reconciler", func() {
 	})
 
 	Context("when the MAPI machine set has MachineAuthority set to Cluster API", func() {
-		JustBeforeEach(func() {
+		BeforeEach(func() {
 			By("Creating the CAPI and MAPI MachineSets")
 			mapiMachineSet = mapiMachineSetBuilder.Build()
-			capiMachineSet = capiMachineSetBuilder.Build()
+			capiMachineSet = capiMachineSetBuilder.WithReplicas(int32(4)).Build()
 
 			Expect(k8sClient.Create(ctx, capiMachineSet)).Should(Succeed())
 			Expect(k8sClient.Create(ctx, mapiMachineSet)).Should(Succeed())
-
-		})
-
-		// For now only happy path
-		It("should update the synchronized condition", func() {
 
 			By("Setting the AuthoritativeAPI to ClusterAPI")
 			Eventually(k.UpdateStatus(mapiMachineSet, func() {
 				mapiMachineSet.Status.AuthoritativeAPI = machinev1beta1.MachineAuthorityClusterAPI
 			})).Should(Succeed())
-
-			Eventually(k.Object(mapiMachineSet), timeout).Should(
-				HaveField("Status.Conditions", ContainElement(
-					SatisfyAll(
-						HaveField("Type", Equal(consts.SynchronizedCondition)),
-						HaveField("Status", Equal(corev1.ConditionTrue)),
-					))),
-			)
 		})
 
+		// For now only happy path
+		It("should update the synchronized condition on the MAPI MachineSet", func() {
+			Eventually(k.Object(mapiMachineSet), timeout).Should(
+				SatisfyAll(
+					HaveField("Status.Conditions", ContainElement(
+						SatisfyAll(
+							HaveField("Type", Equal(consts.SynchronizedCondition)),
+							HaveField("Status", Equal(corev1.ConditionTrue)),
+						))),
+					HaveField("Status.SynchronizedGeneration", Equal(capiMachineSet.GetGeneration())),
+				))
+		})
+
+		It("should update the replica count", func() {
+			Eventually(k.Object(mapiMachineSet), timeout).Should(
+				HaveField("Spec.Replicas", Equal(ptr.To(int32(4)))),
+			)
+		})
 	})
 })
