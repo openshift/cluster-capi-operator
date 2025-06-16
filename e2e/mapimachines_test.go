300476623bdfca4a9315be402b99975073aaa20c
Milind Yadav <miyadav@redhat.com>
2025-06-16 18:15:14 +0530

    e2e test for mapimachine creation when authoritativeAPI is machineapi
    statusAPI should be updated to reflect correct authority
    Synchronized condition is set
    Synchronized generation is set correctly
    Expect a paused CAPI Machine to be created to reflect the MAPI Machines
    Expect to be able to fetch an InfraMachine

---
+++ e2e/mapimachines_test.go
@@ -0,0 +1,173 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+
+	configv1 "github.com/openshift/api/config/v1"
+	machinev1 "github.com/openshift/api/machine/v1beta1"
+	"github.com/openshift/cluster-capi-operator/e2e/framework"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	machineAPINamespace = "openshift-machine-api"
+	clusterAPINamespace = "openshift-cluster-api"
+	pausedConditionType = "Paused"
+	RoleLabel           = "machine.openshift.io/cluster-api-machine-role"
+	DefaultTimeout      = 5 * time.Minute
+	DefaultInterval     = 10 * time.Second
+)
+
+var _ = Describe("[sig-cluster-lifecycle][OCPFeatureGate:MachineAPIMigration] Machines Migration Tests", Ordered, func() {
+	BeforeAll(func() {
+		if platform != configv1.AWSPlatformType {
+			Skip(fmt.Sprintf("Skipping tests on %s, this only support on aws", platform))
+		}
+
+		if !framework.IsMachineAPIMigrationEnabled(ctx, cl) {
+			Skip("Skipping, this feature is only supported on MachineAPIMigration enabled clusters")
+		}
+	})
+
+	var _ = Describe("Mapi machine creation E2E", Ordered, func() {
+		var newMachine *machinev1.Machine
+		// This cleanup runs after each test spec.
+		AfterEach(func() {
+			By("Cleaning up created machine")
+			// If a new machine was created in the test, delete it.
+			if newMachine != nil {
+				err := cl.Delete(context.Background(), newMachine)
+				Expect(client.IgnoreNotFound(err)).To(Succeed(), "Failed to delete the new machine")
+				// Wait for the machine to be fully deleted.
+				Eventually(func() error {
+					key := client.ObjectKey{Name: newMachine.Name, Namespace: newMachine.Namespace}
+					err := cl.Get(context.Background(), key, &machinev1.Machine{})
+					if apierrors.IsNotFound(err) {
+						return nil
+					}
+					return err
+				}, DefaultTimeout, DefaultInterval).Should(Succeed(), "New machine was not deleted")
+			}
+		})
+
+		It("should create a new machine from an existing one,when NO existing CAPI machine and authoritativeAPI is mapi", func() {
+			ctx := context.Background()
+			machineList := &machinev1.MachineList{}
+			By(fmt.Sprintf("Listing worker machines in namespace: %s", machineAPINamespace))
+			workerLabelSelector := client.MatchingLabels{RoleLabel: "worker"}
+			err := cl.List(ctx, machineList, client.InNamespace(machineAPINamespace), workerLabelSelector)
+			Expect(err).NotTo(HaveOccurred(), "Failed to list worker machines")
+			Expect(machineList.Items).NotTo(BeEmpty(), "No worker machines found in the namespace to use as a template")
+			templateMachine := machineList.Items[0]
+			By(fmt.Sprintf("Using worker machine %s as a template", templateMachine.Name))
+
+			if templateMachine.Spec.AuthoritativeAPI == "ClusterAPI" {
+				Skip("This test is for AuthoritativeAPI as MachineAPI")
+			}
+			// Define the new machine based on the template.
+			newMachine = &machinev1.Machine{
+				ObjectMeta: metav1.ObjectMeta{
+					// Using a predictable name based on the testcase in polarion.
+					Name:      fmt.Sprintf("%s-test-%s", templateMachine.Name, "ocp-81829"),
+					Namespace: templateMachine.Namespace,
+					Labels:    templateMachine.Labels,
+				},
+				Spec: *templateMachine.Spec.DeepCopy(),
+			}
+			// Clear status and other instance-specific fields that should not be copied.
+			newMachine.Spec.ProviderID = nil
+			newMachine.ObjectMeta.Labels = nil
+			By(fmt.Sprintf("Creating a new machine in namespace: %s", newMachine.Namespace))
+			// Create the new machine object in the cluster.
+			err = cl.Create(ctx, newMachine)
+			Expect(err).NotTo(HaveOccurred(), "Failed to create new machine")
+			By("Waiting for the new machine to enter 'Running' phase")
+			// Poll the machine's status until it becomes "Running" or a timeout is reached.
+			err = wait.PollUntilContextTimeout(ctx, DefaultInterval, DefaultTimeout, true, func(ctx context.Context) (bool, error) {
+				key := client.ObjectKey{Name: newMachine.Name, Namespace: newMachine.Namespace}
+				currentMachine := &machinev1.Machine{}
+				if err := cl.Get(ctx, key, currentMachine); err != nil {
+					// A NotFound error is fatal, as the machine should not be deleted.
+					if apierrors.IsNotFound(err) {
+						GinkgoLogr.Error(err, "Machine was deleted unexpectedly, failing test.", "name", key.Name)
+						return false, err
+					}
+					// For other transient errors, log and retry.
+					GinkgoLogr.Error(err, "Failed to get machine, retryin", "name", key.Name)
+					return false, nil
+				}
+				currentPhase := "Unknown"
+				if currentMachine.Status.Phase != nil {
+					currentPhase = *currentMachine.Status.Phase
+				}
+				GinkgoLogr.Info("Polling machine status", "name", currentMachine.Name, "phase", currentPhase)
+				if currentPhase == "Running" {
+					GinkgoLogr.Info("Machine entered running phase.", "machineName", currentMachine.Name)
+					newMachine = currentMachine
+					return true, nil
+				}
+				return false, nil
+			})
+			Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("Machine %s did not enter 'Running' phase within the timeout", newMachine.Name))
+			By("Verifying the new machine has a node reference")
+			Eventually(func(g Gomega) {
+				Expect(newMachine.Status.NodeRef).NotTo(BeNil(), "Machine should have a node reference")
+			})
+
+			By("Verifying status of mapi machine for synchronisation done successfully")
+			statusMapiMachine, err := json.Marshal(newMachine.Status)
+			Expect(err).NotTo(HaveOccurred())
+			statusString := string(statusMapiMachine)
+			Expect(statusString).To(ContainSubstring(`"type":"Synchronized"`))
+			Expect(statusString).To(ContainSubstring(`"status":"True"`))
+			Expect(statusString).To(ContainSubstring("Successfully synchronized MAPI Machine to CAPI"))
+			Expect(statusString).To(ContainSubstring("The AuthoritativeAPI is set to MachineAPI"))
+
+			By("Verifying the corresponding machine in openshift-cluster-api is paused")
+			// Get all CAPI machines.
+			allMachines, err := framework.GetMachines(cl)
+			Expect(err).NotTo(HaveOccurred(), "Failed to get CAPI machines")
+			// Find the CAPI machine that corresponds to our new MAPI machine by name.
+			var targetMachine *clusterv1.Machine
+			for _, capiMachine := range allMachines {
+				if capiMachine.Name == newMachine.Name {
+					targetMachine = capiMachine
+					break
+				}
+			}
+			Expect(targetMachine).NotTo(BeNil(), "Could not find the corresponding CAPI machine")
+			statusCapiMachine, err := json.Marshal(targetMachine.Status)
+			Expect(err).NotTo(HaveOccurred())
+			statusString = string(statusCapiMachine)
+			Expect(statusString).To(ContainSubstring(`"type":"Paused"`))
+			Expect(statusString).To(ContainSubstring(`"status":"True"`))
+			Expect(statusString).To(ContainSubstring("Machine has the cluster.x-k8s.io/paused annotation"))
+		})
+	})
+})
