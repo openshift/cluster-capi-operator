f961faa8d2097107a03ad58a26eb9d2e85b009aa
Matthew Booth <mbooth@redhat.com>
2025-06-20 10:28:26 +0100

    Scaffolding for crd-compatibility-checker

---
+++ cmd/crd-compatibility-checker/main.go
@@ -0,0 +1,125 @@
+// Copyright 2024 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/spf13/pflag"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/component-base/config"
+	"k8s.io/component-base/config/options"
+	klog "k8s.io/klog/v2"
+	"k8s.io/klog/v2/textlogger"
+
+	operatorv1alpha1 "github.com/openshift/api/operator/v1alpha1"
+	"github.com/openshift/cluster-capi-operator/pkg/controllers/crdcompatibility"
+	"github.com/openshift/cluster-capi-operator/pkg/util"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+)
+
+func initScheme(scheme *runtime.Scheme) {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(operatorv1alpha1.AddToScheme(scheme))
+}
+
+func main() {
+	scheme := runtime.NewScheme()
+	initScheme(scheme)
+
+	leaderElectionConfig := config.LeaderElectionConfiguration{
+		LeaderElect:       true,
+		LeaseDuration:     util.LeaseDuration,
+		RenewDeadline:     util.RenewDeadline,
+		RetryPeriod:       util.RetryPeriod,
+		ResourceName:      "crd-compatibility-checker-leader",
+		ResourceNamespace: "openshift-cluster-api",
+	}
+
+	healthAddr := flag.String(
+		"health-addr",
+		":9441",
+		"The address for health checking.",
+	)
+
+	logToStderr := flag.Bool(
+		"logtostderr",
+		true,
+		"log to standard error instead of files",
+	)
+
+	textLoggerConfig := textlogger.NewConfig()
+	textLoggerConfig.AddFlags(flag.CommandLine)
+	ctrl.SetLogger(textlogger.NewLogger(textLoggerConfig))
+
+	// Once all the flags are registered, switch to pflag
+	// to allow leader election flags to be bound
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	options.BindLeaderElectionFlags(&leaderElectionConfig, pflag.CommandLine)
+	pflag.Parse()
+
+	if logToStderr != nil {
+		klog.LogToStderr(*logToStderr)
+	}
+
+	cfg := ctrl.GetConfigOrDie()
+
+	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme:                  scheme,
+		HealthProbeBindAddress:  *healthAddr,
+		LeaderElectionNamespace: leaderElectionConfig.ResourceNamespace,
+		LeaderElection:          leaderElectionConfig.LeaderElect,
+		LeaseDuration:           &leaderElectionConfig.LeaseDuration.Duration,
+		LeaderElectionID:        leaderElectionConfig.ResourceName,
+		RetryPeriod:             &leaderElectionConfig.RetryPeriod.Duration,
+		RenewDeadline:           &leaderElectionConfig.RenewDeadline.Duration,
+	})
+	if err != nil {
+		klog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	// Setup the CRD compatibility controller
+	if err := (&crdcompatibility.CRDCompatibilityReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		klog.Error(err, "unable to create controller", "controller", "CRDCompatibility")
+		os.Exit(1)
+	}
+
+	// Setup health checks
+	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
+		klog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+
+	if err := mgr.AddReadyzCheck("check", healthz.Ping); err != nil {
+		klog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	klog.Info("Starting CRD compatibility checker manager")
+
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		klog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
