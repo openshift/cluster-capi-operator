b393a1b427d1e5c1f84b7258f505e3352d6422e1
Matthew Booth <mbooth@redhat.com>
2025-08-08 15:35:41 +0100

    Initial compatible condition

--- pkg/controllers/crdcompatibility/reconcile.go
+++ pkg/controllers/crdcompatibility/reconcile.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2025 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package crdcompatibility
 
 import (
@@ -7,6 +23,7 @@ import (
 
 	"github.com/go-logr/logr"
 	operatorv1alpha1 "github.com/openshift/api/operator/v1alpha1"
+	"github.com/openshift/cluster-capi-operator/pkg/controllers/crdcompatibility/crdchecker"
 	"github.com/openshift/cluster-capi-operator/pkg/util"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -25,6 +42,9 @@ type reconcileState struct {
 
 	compatibilityCRD *apiextensionsv1.CustomResourceDefinition
 	currentCRD       *apiextensionsv1.CustomResourceDefinition
+
+	compatibilityErrors   []string
+	compatibilityWarnings []string
 }
 
 // Reconcile handles the reconciliation of CRDCompatibilityRequirement resources
@@ -90,6 +110,18 @@ func (r *reconcileState) fetchCurrentCRD(ctx context.Context, log logr.Logger, c
 	return nil
 }
 
+func (r *reconcileState) checkCRDCompatibility() error {
+	if r.compatibilityCRD == nil || r.currentCRD == nil {
+		return nil
+	}
+
+	errors, warnings, err := crdchecker.CheckCRDCompatibility(r.compatibilityCRD, r.currentCRD)
+	r.compatibilityErrors = errors
+	r.compatibilityWarnings = warnings
+
+	return err
+}
+
 func (r *reconcileState) reconcileCreateOrUpdate(ctx context.Context, crdCompatibilityRequirement *operatorv1alpha1.CRDCompatibilityRequirement) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
 
@@ -98,10 +130,15 @@ func (r *reconcileState) reconcileCreateOrUpdate(ctx context.Context, crdCompati
 	err := errors.Join(
 		r.parseCompatibilityCRD(crdCompatibilityRequirement),
 		r.fetchCurrentCRD(ctx, logger, crdCompatibilityRequirement),
+		r.checkCRDCompatibility(),
 	)
-
 	if err != nil {
 		return ctrl.Result{}, err
+	}
+
+	if r.compatibilityCRD == nil {
+		// Should have been handled by API validation
+		return ctrl.Result{}, util.NoRequeueError(fmt.Errorf("compatibilityCRD was not provided"), noRequeueErrorReasonInvalidCompatibilityCRD)
 	}
 
 	// TODO: Implement reconciliation logic
