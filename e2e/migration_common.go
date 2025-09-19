95570120fd83bbe817ad49ddb112045574c8d2e4
Zhaohua Sun <zhsun@redhat.com>
2025-09-19 09:41:04 +0800

    refactor: Move migration test constants to migration_common.go

---
+++ e2e/migration_common.go
@@ -0,0 +1,12 @@
+package e2e
+
+import (
+	machinev1beta1 "github.com/openshift/api/machine/v1beta1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+)
+
+const (
+	SynchronizedCondition machinev1beta1.ConditionType = "Synchronized"
+	MAPIPausedCondition   machinev1beta1.ConditionType = "Paused"
+	CAPIPausedCondition                                = clusterv1.PausedV1Beta2Condition
+)
