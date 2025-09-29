9f446a788c551e5a10dc327a625cd09326cb83ae
Damiano Donati <damiano.donati@gmail.com>
2025-09-29 10:54:20 +0200

    fix: return nil ownerrefs when empty

--- pkg/controllers/machinesync/machine_sync_controller.go
+++ pkg/controllers/machinesync/machine_sync_controller.go
@@ -917,6 +917,10 @@ func (r *MachineSyncReconciler) convertMAPIMachineOwnerReferencesToCAPI(ctx cont
 func (r *MachineSyncReconciler) convertCAPIMachineOwnerReferencesToMAPI(ctx context.Context, capiMachine *clusterv1.Machine) ([]metav1.OwnerReference, error) {
 	mapiOwnerReferences := []metav1.OwnerReference{}
 
+	if capiMachine.OwnerReferences == nil {
+		return nil, nil
+	}
+
 	if len(capiMachine.OwnerReferences) == 0 {
 		return mapiOwnerReferences, nil
 	}
@@ -964,7 +968,7 @@ func (r *MachineSyncReconciler) convertCAPIMachineOwnerReferencesToMAPI(ctx cont
 		}
 
 		// MAPI doesn't have a cluster, so we don't need to add an owner reference.
-		return mapiOwnerReferences, nil
+		return nil, nil
 	default:
 		return nil, field.Invalid(field.NewPath("metadata", "ownerReferences"), capiMachine.OwnerReferences, errUnsuportedOwnerKindForConversion.Error())
 	}
