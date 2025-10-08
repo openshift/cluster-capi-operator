630c5de72eb26941d37b64885179104dc3e97605
Radek Maňák <Radek.Manak@protonmail.com>
2025-10-08 16:57:49 +0200

    Add test converting control plane machine; only owner reference, no role

--- pkg/conversion/mapi2capi/aws_test.go
+++ pkg/conversion/mapi2capi/aws_test.go
@@ -131,6 +131,28 @@ var _ = Describe("mapi2capi AWS conversion", func() {
 			expectedErrors:   []string{},
 			expectedWarnings: []string{},
 		}),
+		Entry("With LoadBalancers on control plane machine with CPMS owner reference; no role label", awsMAPI2CAPIConversionInput{
+			machineBuilder: awsMAPIMachineBase.
+				WithOwnerReferences([]metav1.OwnerReference{
+					{
+						APIVersion: mapiv1beta1.SchemeGroupVersion.String(),
+						Kind:       "ControlPlaneMachineSet",
+						Name:       "cluster",
+						UID:        "cpms-uid",
+					},
+				}).
+				WithProviderSpecBuilder(
+					awsBaseProviderSpec.WithLoadBalancers(
+						[]mapiv1beta1.LoadBalancerReference{
+							{Name: "ci-ln-d2q97ct-76ef8-httpb-int", Type: mapiv1beta1.NetworkLoadBalancerType},
+							{Name: "ci-ln-d2q97ct-76ef8-httpb-ext", Type: mapiv1beta1.NetworkLoadBalancerType},
+						},
+					),
+				),
+			infra:            infra,
+			expectedErrors:   []string{},
+			expectedWarnings: []string{},
+		}),
 		Entry("With DeviceIndex non-zero", awsMAPI2CAPIConversionInput{
 			machineBuilder: awsMAPIMachineBase.WithProviderSpecBuilder(
 				awsBaseProviderSpec.WithDeviceIndex(1),
