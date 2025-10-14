05c494592c9160436cf2e560b6a0ae1c2e75a2fc
Zhaohua Sun <zhsun@redhat.com>
2025-10-14 15:56:16 +0800

    machineset/machine status conversion e2e

--- e2e/migration_common.go
+++ e2e/migration_common.go
@@ -1,6 +1,9 @@
 package e2e
 
 import (
+	"fmt"
+	"time"
+
 	mapiv1beta1 "github.com/openshift/api/machine/v1beta1"
 	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
 )
@@ -13,3 +16,9 @@ const (
 	// CAPIPausedCondition represents the paused state for CAPI machines.
 	CAPIPausedCondition = clusterv1.PausedCondition
 )
+
+// UniqueName generates a unique name with the given prefix using nanosecond timestamp.
+// This is useful for creating resources in parallel tests without naming conflicts.
+func UniqueName(prefix string) string {
+	return fmt.Sprintf("%s-%d", prefix, time.Now().UnixNano())
+}
