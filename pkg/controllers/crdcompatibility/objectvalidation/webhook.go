2bf1e0e48a4044d1b7e5a561da06c7b23bcc19ec
Christian Schlotter <christi.schlotter@gmail.com>
2025-10-28 15:42:05 +0100

    crd-compatibility-checker: CR Object validator: first implementation WIP

--- pkg/controllers/crdcompatibility/objectvalidation/webhook.go
+++ pkg/controllers/crdcompatibility/objectvalidation/webhook.go
@@ -16,17 +16,32 @@ package objectvalidation
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
+	apiextensionsv1alpha1 "github.com/openshift/api/apiextensions/v1alpha1"
+	apiextensionshelpers "k8s.io/apiextensions-apiserver/pkg/apihelpers"
+	apiextensionsinternal "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	structuralschema "k8s.io/apiextensions-apiserver/pkg/apiserver/schema"
+	"k8s.io/apiextensions-apiserver/pkg/apiserver/schema/cel"
+	apiextensionsvalidation "k8s.io/apiextensions-apiserver/pkg/apiserver/validation"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	celconfig "k8s.io/apiserver/pkg/apis/cel"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+	"sigs.k8s.io/yaml"
 )
 
 const (
 	webhookPrefix = "/crdcompatibility/"
+)
+
+var (
+	errObjectValidator = errors.New("failed to create the object validator")
 )
 
 var _ admission.Handler = &validator{}
@@ -61,15 +76,106 @@ func (v *validator) SetupWithManager(ctx context.Context, mgr ctrl.Manager, opts
 
 // ValidateCreate validates the creation of an object.
 func (v *validator) ValidateCreate(ctx context.Context, compatibilityRequirementName string, obj *unstructured.Unstructured) (warnings admission.Warnings, err error) {
-	return nil, nil
+	validator, celValidator, err := v.createSchemaValidator(ctx, compatibilityRequirementName, obj.GroupVersionKind().Version)
+	if err != nil {
+		return nil, fmt.Errorf("failed to validate: %w", err)
+	}
+
+	res := validator.Validate(obj)
+	if !res.IsValid() && res.HasWarnings() {
+		for _, warning := range res.Warnings {
+			warnings = append(warnings, warning.Error())
+		}
+	}
+
+	// TODO: should we
+
+	if celErrs, _ := celValidator.Validate(ctx, nil, nil, obj.Object, nil, celconfig.RuntimeCELCostBudget); celErrs != nil {
+		res.Errors = append(res.Errors, celErrs.ToAggregate())
+	}
+
+	return warnings, fmt.Errorf("validation failed: %w", res.AsError())
 }
 
 // ValidateUpdate validates the update of an object.
 func (v *validator) ValidateUpdate(ctx context.Context, compatibilityRequirementName string, oldObj, obj *unstructured.Unstructured) (warnings admission.Warnings, err error) {
-	return nil, nil
+	validator, celValidator, err := v.createSchemaValidator(ctx, compatibilityRequirementName, obj.GroupVersionKind().Version)
+	if err != nil {
+		return nil, fmt.Errorf("failed to validate: %w", err)
+	}
+
+	res := validator.ValidateUpdate(obj, oldObj)
+	if !res.IsValid() && res.HasWarnings() {
+		for _, warning := range res.Warnings {
+			warnings = append(warnings, warning.Error())
+		}
+	}
+
+	if celErrs, _ := celValidator.Validate(ctx, nil, nil, obj, nil, celconfig.RuntimeCELCostBudget); celErrs != nil {
+		res.Errors = append(res.Errors, celErrs.ToAggregate())
+	}
+
+	return warnings, fmt.Errorf("validation failed: %w", res.AsError())
 }
 
 // ValidateDelete validates the deletion of an object.
 func (v *validator) ValidateDelete(ctx context.Context, compatibilityRequirementName string, obj *unstructured.Unstructured) (warnings admission.Warnings, err error) {
 	return nil, nil
+}
+
+// FIXME(chrischdi):
+// Should we use NewStrategy instead and its public functions? or do we need to adapt some more from their pub funcs
+// https://github.com/kubernetes/kubernetes/blob/ebc1ccc491c944fa0633f147698e0dc02675051d/staging/src/k8s.io/apiextensions-apiserver/pkg/registry/customresource/strategy.go#L76
+func (v *validator) createSchemaValidator(ctx context.Context, compatibilityRequirementName string, version string) (apiextensionsvalidation.SchemaValidator, *cel.Validator, error) {
+	// Get the CompatibilityRequirement
+	compatibilityRequirement := &apiextensionsv1alpha1.CompatibilityRequirement{}
+	if err := v.client.Get(ctx, client.ObjectKey{Name: compatibilityRequirementName}, compatibilityRequirement); err != nil {
+		return nil, nil, fmt.Errorf("failed to get CompatibilityRequirement %q: %w", compatibilityRequirementName, err)
+	}
+
+	// Extract the CRD so we can use the schema.
+	compatibilityCRD := &apiextensionsv1.CustomResourceDefinition{}
+	if err := yaml.Unmarshal([]byte(compatibilityRequirement.Spec.CompatibilitySchema.CustomResourceDefinition.Data), &compatibilityCRD); err != nil {
+		return nil, nil, fmt.Errorf("failed to parse compatibilityCRD for CompatibilityRequirement %q: %w", compatibilityRequirement.Name, err)
+	}
+
+	if compatibilityCRD.APIVersion != "apiextensions.k8s.io/v1" || compatibilityCRD.Kind != "CustomResourceDefinition" {
+		return nil, nil, fmt.Errorf("%w: expected APIVersion to be apiextensions.k8s.io/v1 and Kind to be CustomResourceDefinition, got %s/%s", errObjectValidator, compatibilityCRD.APIVersion, compatibilityCRD.Kind)
+	}
+
+	// Adapted from k8s.io/apiextensions-apiserver/pkg/apiserver/customresource_handler.go getOrCreateServingInfoFor.
+	// Creates the validator from JSONSchemaProps.
+	validationSchema, err := apiextensionshelpers.GetSchemaForVersion(compatibilityCRD, version)
+	if err != nil {
+		return nil, nil, fmt.Errorf("the server could not properly serve the CR schema: %w", err)
+	} else if validationSchema == nil {
+		return nil, nil, fmt.Errorf("%w: validationSchema can't be nil", errObjectValidator)
+	}
+
+	var internalSchemaProps *apiextensionsinternal.JSONSchemaProps
+
+	var internalValidationSchema *apiextensionsinternal.CustomResourceValidation
+
+	if validationSchema != nil {
+		internalValidationSchema = &apiextensionsinternal.CustomResourceValidation{}
+		if err := apiextensionsv1.Convert_v1_CustomResourceValidation_To_apiextensions_CustomResourceValidation(validationSchema, internalValidationSchema, nil); err != nil {
+			return nil, nil, fmt.Errorf("failed to convert CRD validation to internal version: %w", err)
+		}
+
+		internalSchemaProps = internalValidationSchema.OpenAPIV3Schema
+	}
+
+	structuralSchema, err := structuralschema.NewStructural(internalValidationSchema.OpenAPIV3Schema)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to convert CRD validation to internal version: %w", err)
+	}
+
+	celValidator := cel.NewValidator(structuralSchema, true, celconfig.PerCallLimit)
+
+	validator, _, err := apiextensionsvalidation.NewSchemaValidator(internalSchemaProps)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create a SchemaValidator: %w", err)
+	}
+
+	return validator, celValidator, nil
 }
