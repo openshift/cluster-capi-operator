71dc42e79fded27fd197078a92f2cc2f398520d2
Joel Speed <joel.speed@hotmail.co.uk>
2026-02-13 15:20:54 +0000

    Bootstrap empty object pruning admission webhook

---
+++ pkg/controllers/crdcompatibility/objectpruning/webhook.go
@@ -0,0 +1,70 @@
+// Copyright 2026 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package objectpruning
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+const (
+	// WebhookPrefix is the static path prefix of our object admission endpoint.
+	// Requests will be sent to a sub-path with the next component of the path
+	// as a compatibility requirement name.
+	WebhookPrefix = "/compatibility-requirement-object-mutation/"
+)
+
+// validator implements the admission.Handler to have a custom Handle function which is able to
+// validate arbitrary objects against CompatibilityRequirements by leveraging unstructured.
+type validator struct {
+	client  client.Reader
+	decoder admission.Decoder
+}
+
+// NewValidator returns a partially initialized ObjectValidator.
+func NewValidator() *validator {
+	return &validator{
+		// This decoder is only used to decode to unstructured and for CompatibilityRequirements.
+		decoder: admission.NewDecoder(runtime.NewScheme()),
+	}
+}
+
+type controllerOption func(*builder.Builder) *builder.Builder
+
+// SetupWithManager registers the objectValidator webhook with an manager.
+func (v *validator) SetupWithManager(ctx context.Context, mgr ctrl.Manager, opts ...controllerOption) error {
+	v.client = mgr.GetClient()
+
+	// Register a webhook on a path with a dynamic component for the compatibility requirement name.
+	// we will extract this component into the context so that the handler can identify which compatibility
+	// requirement the request was intended to validate against.
+	mgr.GetWebhookServer().Register(WebhookPrefix+"{CompatibilityRequirement}", &admission.Webhook{
+		Handler:         v,
+		WithContextFunc: compatibilityRequrementIntoContext,
+	})
+
+	return nil
+}
+
+// handleObjectPruning handles the pruning of an object.
+func (v *validator) handleObjectPruning(ctx context.Context, compatibilityRequirementName string, obj *unstructured.Unstructured) error {
+	return nil
+}
