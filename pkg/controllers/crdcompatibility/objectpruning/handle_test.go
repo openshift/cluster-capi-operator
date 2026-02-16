af8cbc184ea194372f51a21ec0f9bb3ff26550be
Joel Speed <joel.speed@hotmail.co.uk>
2026-02-16 14:57:44 +0000

    Switch to apiextensions pruning implementation

--- pkg/controllers/crdcompatibility/objectpruning/handle_test.go
+++ pkg/controllers/crdcompatibility/objectpruning/handle_test.go
@@ -396,7 +396,6 @@ func createStrictCRDSchema() *apiextensionsv1.CustomResourceDefinition {
 			},
 		},
 	}
-	crd.Spec.Versions[0].Schema.OpenAPIV3Schema.XPreserveUnknownFields = ptr.To(false)
 
 	return crd
 }
