bc07c17b5d43edc88070927d4c38e8e707558458
Damiano Donati <damiano.donati@gmail.com>
2026-09-11 12:06:58 +0200

    test(e2e): verify kubeconfig repair

--- e2e/cluster_api_machine_management.go
+++ e2e/cluster_api_machine_management.go
@@ -120,6 +120,46 @@ var _ = Describe("[sig-cluster-lifecycle][OCPFeatureGate:ClusterAPIMachineManage
 			Expect(user).NotTo(BeNil())
 			Expect(user.TokenFile).To(Equal("/var/run/secrets/kubernetes.io/serviceaccount/token"))
 			Expect(user.Token).To(BeEmpty())
+			context := kubeconfig.Contexts["management-cluster"]
+			Expect(context).NotTo(BeNil())
+			Expect(context.Cluster).To(Equal("management-cluster"))
+			Expect(context.AuthInfo).To(Equal("service-account"))
+			Expect(kubeconfig.CurrentContext).To(Equal("management-cluster"))
+		})
+
+		It("should repair a mutated management cluster kubeconfig Secret", func() {
+			key := client.ObjectKey{Namespace: framework.CAPINamespace, Name: fmt.Sprintf("%s-kubeconfig", clusterName)}
+			secret := &corev1.Secret{}
+			Eventually(func() error { return cl.Get(ctx, key, secret) }).WithTimeout(framework.WaitMedium).WithPolling(framework.RetryMedium).Should(Succeed())
+			expectedValue := append([]byte(nil), secret.Data["value"]...)
+			secret.Data["value"] = []byte("invalid-kubeconfig")
+			Expect(cl.Update(ctx, secret)).To(Succeed())
+
+			By("waiting for the revision installer to restore the kubeconfig")
+			Eventually(func() []byte {
+				current := &corev1.Secret{}
+				if err := cl.Get(ctx, key, current); err != nil {
+					return nil
+				}
+				return current.Data["value"]
+			}).WithTimeout(framework.WaitMedium).WithPolling(framework.RetryMedium).Should(Equal(expectedValue))
+		})
+
+		It("should recreate a deleted management cluster kubeconfig Secret", func() {
+			key := client.ObjectKey{Namespace: framework.CAPINamespace, Name: fmt.Sprintf("%s-kubeconfig", clusterName)}
+			secret := &corev1.Secret{}
+			Eventually(func() error { return cl.Get(ctx, key, secret) }).WithTimeout(framework.WaitMedium).WithPolling(framework.RetryMedium).Should(Succeed())
+			expectedValue := append([]byte(nil), secret.Data["value"]...)
+			Expect(cl.Delete(ctx, secret)).To(Succeed())
+
+			By("waiting for the revision installer to recreate the kubeconfig")
+			Eventually(func() []byte {
+				current := &corev1.Secret{}
+				if err := cl.Get(ctx, key, current); err != nil {
+					return nil
+				}
+				return current.Data["value"]
+			}).WithTimeout(framework.WaitMedium).WithPolling(framework.RetryMedium).Should(Equal(expectedValue))
 		})
 
 		It("should not have the removed capi-controllers token Secret", func() {
